6ea1deb40422ec7cebb956ef0753533fb1005d5d
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-09-25 19:30:01 +0530

    Screens

---
+++ app/src/main/java/com/example/medicalapp/onboardingscreen/OnboardingScreen.kt
@@ -0,0 +1,12 @@
+package com.example.medicalapp.onboardingscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.medicalapp.R
+
+class OnboardingScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_screen)
+    }
+}
