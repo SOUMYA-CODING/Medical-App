19bb5be3679d48b326315de520ec1a91aef2cb1c
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-09-25 19:39:41 +0530

    Update

--- app/src/main/java/com/example/medicalapp/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/medicalapp/splashscreen/SplashScreen.kt
@@ -1,11 +1,12 @@
-package com.example.medicalapp
+package com.example.medicalapp.splashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.medicalapp.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
