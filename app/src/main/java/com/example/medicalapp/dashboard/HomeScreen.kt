9d4f2b35b70c6a83ecc12ac6a0e90879da2c9390
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-09-25 19:32:21 +0530

    Update

---
+++ app/src/main/java/com/example/medicalapp/dashboard/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.medicalapp.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.medicalapp.R
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
