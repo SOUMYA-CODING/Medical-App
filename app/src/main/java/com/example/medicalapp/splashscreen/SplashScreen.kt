8118d1bff90f0004d5629204304239aa04904bf0
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-09-27 10:07:25 +0530

    Update

--- app/src/main/java/com/example/medicalapp/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/example/medicalapp/splashscreen/SplashScreen.kt
@@ -1,12 +1,23 @@
 package com.example.medicalapp.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.medicalapp.R
+import com.example.medicalapp.onboardingscreen.OnboardingScreen
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        // Splash Screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, OnboardingScreen::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 }
